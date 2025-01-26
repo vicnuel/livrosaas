3e30eb5cdeff964dc0cc7cf457cf3e9445118f51
Victor Emanuel <78376383+vicnuel@users.noreply.github.com>
2025-01-26 13:22:05 -0300

    add turso sqlite

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const db = globalThis.prisma || new PrismaClient();
+const libsql = createClient({
+  url: process.env.TURSO_DATABASE_URL || process.env.DATABASE_URL || "",
+  authToken: process.env.TURSO_AUTH_TOKEN,
+});
+
+const adapter = new PrismaLibSQL(libsql);
+
+const db = globalThis.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
 
